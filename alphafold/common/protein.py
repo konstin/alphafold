d3906ef108d0e4b221d70d08a44d3e008bfac123
Martin Steinegger <themartinsteinegger@gmail.com>
2021-11-08 02:02:50 +0900

    - Remove seen_sequence from make_msa_features to avoid paired sequences to be removed - change Protein.to_pdb back to default implementation

--- alphafold/common/protein.py
+++ alphafold/common/protein.py
@@ -143,6 +143,7 @@ def _chain_end(atom_index, end_resname, chain_name, residue_index) -> str:
           f'{chain_name:>1}{residue_index:>4}')
 
 
+
 def to_pdb(prot: Protein) -> str:
   """Converts a `Protein` instance to a PDB string.
   Args:
@@ -160,28 +161,34 @@ def to_pdb(prot: Protein) -> str:
   aatype = prot.aatype
   atom_positions = prot.atom_positions
   residue_index = prot.residue_index.astype(np.int32)
+  chain_index = prot.chain_index.astype(np.int32)
   b_factors = prot.b_factors
 
   if np.any(aatype > residue_constants.restype_num):
     raise ValueError('Invalid aatypes.')
 
+  # Construct a mapping from chain integer indices to chain ID strings.
+  chain_ids = {}
+  for i in np.unique(chain_index):  # np.unique gives sorted output.
+    if i >= PDB_MAX_CHAINS:
+      raise ValueError(
+          f'The PDB format supports at most {PDB_MAX_CHAINS} chains.')
+    chain_ids[i] = PDB_CHAIN_IDS[i]
+
   pdb_lines.append('MODEL     1')
   atom_index = 1
-  chain_index = -1
-  residue_index_prev = residue_index[0]-100
+  last_chain_index = chain_index[0]
   # Add all atom sites.
   for i in range(aatype.shape[0]):
-    if residue_index[i] - residue_index_prev > 1:   # chain break
-      chain_index += 1
-      res_num = 1
-      if chain_index > 0:
-        pdb_lines.append("TER")
-    elif residue_index[i] != residue_index_prev:
-      res_num += 1
-    residue_index_prev = residue_index[i]
-    chain_id = CHAIN_IDs[chain_index]
+    # Close the previous chain if in a multichain PDB.
+    if last_chain_index != chain_index[i]:
+      pdb_lines.append(_chain_end(
+          atom_index, res_1to3(aatype[i - 1]), chain_ids[chain_index[i - 1]],
+          residue_index[i - 1]))
+      last_chain_index = chain_index[i]
+      atom_index += 1  # Atom index increases at the TER symbol.
+
     res_name_3 = res_1to3(aatype[i])
-    #
     for atom_name, pos, mask, b_factor in zip(
         atom_types, atom_positions[i], atom_mask[i], b_factors[i]):
       if mask < 0.5:
@@ -196,24 +203,23 @@ def to_pdb(prot: Protein) -> str:
       charge = ''
       # PDB is a columnar format, every space matters here!
       atom_line = (f'{record_type:<6}{atom_index:>5} {name:<4}{alt_loc:>1}'
-                   f'{res_name_3:>3} {chain_id:>1}'
-                   f'{res_num:>4}{insertion_code:>1}   '
+                   f'{res_name_3:>3} {chain_ids[chain_index[i]]:>1}'
+                   f'{residue_index[i]:>4}{insertion_code:>1}   '
                    f'{pos[0]:>8.3f}{pos[1]:>8.3f}{pos[2]:>8.3f}'
                    f'{occupancy:>6.2f}{b_factor:>6.2f}          '
                    f'{element:>2}{charge:>2}')
       pdb_lines.append(atom_line)
       atom_index += 1
 
-  # Close the chain.
-  chain_end = 'TER'
-  chain_termination_line = (
-      f'{chain_end:<6}{atom_index:>5}      {res_1to3(aatype[-1]):>3} '
-      f'{chain_id:>1}{residue_index[-1]:>4}')
-  pdb_lines.append(chain_termination_line)
-    
-  pdb_lines.append('')
-  return '\n'.join(pdb_lines)
+  # Close the final chain.
+  pdb_lines.append(_chain_end(atom_index, res_1to3(aatype[-1]),
+                              chain_ids[chain_index[-1]], residue_index[-1]))
+  pdb_lines.append('ENDMDL')
+  pdb_lines.append('END')
 
+  # Pad all lines to 80 characters.
+  pdb_lines = [line.ljust(80) for line in pdb_lines]
+  return '\n'.join(pdb_lines) + '\n'  # Add terminating newline.
 
 def ideal_atom_mask(prot: Protein) -> np.ndarray:
   """Computes an ideal atom mask.
